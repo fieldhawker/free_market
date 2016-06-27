a4c8eab7200dbf5e44f30cff192a4130d5d0d6a2
fieldhawker <fieldhawker+github@gmail.com>
2016-06-27 16:03:05 +0900

    Add files via upload
    ソースファイルの初期設定用のshを追加

---
+++ 20_www/init.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+echo "開発環境の初期設定を開始しますか？ [Y/n]"
+read ANSWER
+
+case $ANSWER in
+
+    "Y" | "y" | "yes" | "Yes" | "YES" ) 
+        echo "YES!!";
+        
+        
+        # vender系のインストール
+        cd /usr/share/nginx/html/free_market
+        composer install
+
+        # envファイル作成
+        cd /usr/share/nginx/html/free_market
+        cp -p .env.example .env
+        php artisan key:generate
+
+        # マイグレーション
+        cd /usr/share/nginx/html/free_market
+        php artisan migrate:refresh --seed
+
+        # シンボリックリンクの生成
+        cd /usr/share/nginx/html/free_market/public
+        ln -s ../vendor/almasaeed2010/adminlte/dist/ dist
+        ln -s ../vendor/almasaeed2010/adminlte/bootstrap/ bootstrap
+        ln -s ../vendor/almasaeed2010/adminlte/plugins/ plugins
+
+        ;;
+
+    * ) 
+        echo "開発環境の初期設定を開始しませんでした";
+        ;;
+
+esac
+
+
