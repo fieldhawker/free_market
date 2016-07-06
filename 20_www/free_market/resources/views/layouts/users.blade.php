7c9024397feff26c0da85e962c5c5e5da7ac47c8
fieldhawker <fieldhawker@gmail.com>
2016-07-06 22:22:59 +0900

    フロント画面のサンプルデザインを反映

--- 20_www/free_market/resources/views/layouts/users.blade.php
+++ 20_www/free_market/resources/views/layouts/users.blade.php
@@ -47,7 +47,7 @@
             <div class="collapse navbar-collapse" id="app-navbar-collapse">
                 <!-- Left Side Of Navbar -->
                 <ul class="nav navbar-nav">
-                    <li><a href="{{ url('/users/home') }}">Home</a></li>
+                    <li><a href="{{ url('/') }}">Home</a></li>
                     <li><a href="{{ url('/admin/home') }}">Admin</a></li>
                 </ul>
 
