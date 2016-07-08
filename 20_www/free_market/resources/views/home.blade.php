08f2e3f799ef1978460a783188375e2c41037cfe
keiji.takano <keiji.takano>
2016-07-08 19:06:07 +0900

    dataTableの設定を追記 画面全体のフォントをWebフォントに変更

--- 20_www/free_market/resources/views/home.blade.php
+++ 20_www/free_market/resources/views/home.blade.php
@@ -23,6 +23,10 @@
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
+    
+    
+    <!-- common -->
+    <link href={{asset("css/common.css?" . date('YmdHis'))}} rel="stylesheet" type="text/css" />
 
 </head>
 
